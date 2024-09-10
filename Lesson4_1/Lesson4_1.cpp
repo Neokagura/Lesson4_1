cd7adcf88b74c3b3af21825c850e4864d5660ac5
Ness <bur>
2024-09-10 19:04:00 +0300

    Добавьте файлы проекта.

---
+++ Lesson4_1/Lesson4_1.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int speed;
+    int height;
+    cout << "Введите скорость самолета: ";
+    cin >> speed;
+    cout << "Введите высоту: ";
+    cin >> height;
+    if (speed >= 750 && speed <= 850 && height>= 9000 && height<= 9500) {
+        cout << "Эшелон верный!";
+    }
+    else {
+        cout << "Эшелон неверный!";
+    }
+}
+
